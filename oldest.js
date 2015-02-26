31d0852c143a138fe040703448c714a8e3cfbd24
Kevin Regner <krw10108@saintjoe.edu>
2015-02-26 14:08:31 -0600

    Update oldest.js

--- oldest.js
+++ oldest.js
@@ -8,9 +8,9 @@ function map(array, transform) {
   return mapped;
 }
 
-var changingCost = ancestry.filter(function(game) {
+var ChangingCost = ancestry.filter(function(game) {
   return game.cost - 2;
 });
-console.log(map(changingCost, function(game) {
+console.log(map(ChangingCost, function(game) {
   return "The cost of " + game.name +  " after a month is: " +  game.cost;
 }));
